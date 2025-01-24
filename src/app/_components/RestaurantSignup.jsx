2e7e0a47ca8df3f4ba86e3e952ffd96a13c8d959
Maroof Qureshi <developermaroof@gmail.com>
2025-01-24 22:27:08 +0500

    Added the Code of Form handling for SignUp.jsx using useState

--- src/app/_components/RestaurantSignup.jsx
+++ src/app/_components/RestaurantSignup.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RestaurantSignup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [restaurantName, setRestaurantName] = useState("");
+  const [city, setCity] = useState("");
+  const [address, setAddress] = useState("");
+  const [contact, setContact] = useState("");
+
+  const handleSignUp = () => {
+    console.log(
+      "SignUp Clicked",
+      email,
+      password,
+      confPassword,
+      restaurantName,
+      city,
+      address,
+      contact
+    );
+  };
+
   return (
     <>
       <h1>SignUp</h1>
@@ -10,6 +31,8 @@ const RestaurantSignup = () => {
             className="input-field"
             type="email"
             placeholder="Enter EmailID"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
@@ -17,6 +40,8 @@ const RestaurantSignup = () => {
             className="input-field"
             type="password"
             placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
@@ -24,6 +49,8 @@ const RestaurantSignup = () => {
             className="input-field"
             type="password"
             placeholder="Confirm Password"
+            value={confPassword}
+            onChange={(e) => setConfPassword(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
@@ -31,16 +58,26 @@ const RestaurantSignup = () => {
             className="input-field"
             type="text"
             placeholder="Enter Restaurant Name"
+            value={restaurantName}
+            onChange={(e) => setRestaurantName(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
-          <input className="input-field" type="text" placeholder="Enter City" />
+          <input
+            className="input-field"
+            type="text"
+            placeholder="Enter City"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
         </div>
         <div className="input-wrapper">
           <input
             className="input-field"
             type="text"
             placeholder="Enter Address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
@@ -48,10 +85,14 @@ const RestaurantSignup = () => {
             className="input-field"
             type="number"
             placeholder="Enter Contact No."
+            value={contact}
+            onChange={(e) => setContact(e.target.value)}
           />
         </div>
         <div className="input-wrapper">
-          <button className="button">SignUp</button>
+          <button className="button" onClick={handleSignUp}>
+            SignUp
+          </button>
         </div>
       </div>
     </>
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,11 @@ export default function Home() {
   return (
     <div>
       <h1>Food App</h1>
+      <ul>
+        <li>
+          <a href="/restaurant"> Restaurant Page</a>
+        </li>
+      </ul>
     </div>
   );
 }
