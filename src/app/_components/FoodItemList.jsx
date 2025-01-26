722aae96628c3fdd688e9c21ec246d287c51cd9a
Maroof Qureshi <developermaroof@gmail.com>
2025-01-26 21:48:04 +0500

    Integrated Api and Displayed Data of foodItems in the Ui

--- src/app/_components/FoodItemList.jsx
+++ src/app/_components/FoodItemList.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const FoodItemList = () => {
+  const [foodItems, setFoodItems] = useState();
+
+  useEffect(() => {
+    loadFoodItems();
+  }, []);
+
+  const loadFoodItems = async () => {
+    const restaurantData = JSON.parse(localStorage.getItem("restaurantUser"));
+    const food_id = restaurantData._id;
+    let response = await fetch(
+      `http://localhost:3000/api/restaurant/foods/${food_id}`
+    );
+    response = await response.json();
+    if (response.success) {
+      setFoodItems(response.result);
+    } else {
+      alert("Couldn't find food items");
+    }
+  };
+
   return (
     <div>
       <h1>Food Items List</h1>
@@ -16,19 +36,22 @@ const FoodItemList = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Pizza</td>
-            <td>$20</td>
-            <td>Delicious Italian Pizza</td>
-            <td>
-              <img src="" alt="Image" />
-            </td>
-            <td>
-              <button>Edit</button>
-              <button>Delete</button>
-            </td>
-          </tr>
+          {foodItems &&
+            foodItems.map((item, key) => (
+              <tr key={key}>
+                <td>{key + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>{item.description}</td>
+                <td>
+                  <img src={item.path} alt="Image" />
+                </td>
+                <td>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
