3503e04d9c89d7fc201dee7aa812dea3f2bbfe46
Maroof Qureshi <developermaroof@gmail.com>
2025-01-30 11:14:42 +0500

    Added Restaurant Location List Api in Customer/Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,30 @@
+"use client";
+import { useEffect, useState } from "react";
 import CustomerHeader from "./_components/CustomerHeader";
 import Footer from "./_components/Footer";
 
 export default function Home() {
+  const [locations, setLocations] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [showLocations, setShowLocations] = useState(false);
+
+  useEffect(() => {
+    loadLocations();
+  }, []);
+
+  const loadLocations = async () => {
+    let response = await fetch("http://localhost:3000/api/customer/locations");
+    response = await response.json();
+    if (response.success) {
+      setLocations(response.result);
+    }
+  };
+
+  const handleListItem = (item) => {
+    setSelectedLocation(item);
+    setShowLocations(false);
+  };
+
   return (
     <div>
       <CustomerHeader />
@@ -12,7 +35,15 @@ export default function Home() {
             className="select-input"
             type="text"
             placeholder="Select Place"
+            value={selectedLocation}
+            onClick={() => setShowLocations(true)}
           />
+          <ul className="location-list">
+            {showLocations &&
+              locations.map((item) => {
+                return <li onClick={() => handleListItem(item)}>{item}</li>;
+              })}
+          </ul>
           <input
             className="search-input"
             type="text"
